071dd1a64a673ded085c76a89752867ae87c3558
FarkleFMR <44482071+FarkleFMR@users.noreply.github.com>
2021-04-15 20:22:39 -0600

    ParseXML Comentado

--- ParseXML-SQL/SQLSERVER PARSE.sql
+++ ParseXML-SQL/SQLSERVER PARSE.sql
@@ -1,45 +1,49 @@
-DECLARE @Datos xml
+DECLARE @Datos xml/*Declaramos la variable Datos como un tipo XML*/
  
-SELECT @Datos = D
+SELECT @Datos = D  /*El select imprime los contenidos del XML para dejarlo cargado en memoria*/
 FROM OPENROWSET (BULK 'D:\DatosXML.xml', SINGLE_BLOB) AS Datos(D)
     
-DECLARE @hdoc int
+DECLARE @hdoc int /*Creamos hdoc que va a ser un identificador*/
     
-EXEC sp_xml_preparedocument @hdoc OUTPUT, @Datos
-INSERT INTO dbo.TipoDocIdent
+EXEC sp_xml_preparedocument @hdoc OUTPUT, @Datos/*Toma el identificador y a la variable con el documento y las asocia*/
+INSERT INTO dbo.TipoDocIdent/*Inserta en la tabla TipoDocIdent*/
 SELECT *
-FROM OPENXML (@hdoc, '/Datos/Tipos_de_Documento_de_Identidad/TipoDocuIdentidad' , 1)
-WITH(
+FROM OPENXML (@hdoc, '/Datos/Tipos_de_Documento_de_Identidad/TipoDocuIdentidad' , 1)/*Lee los contenidos del XML y para eso necesita un identificador,el 
+PATH del nodo y el 1 que sirve para retornar solo atributos*/
+WITH(/*Dentro del WITH se pone el nombre y el tipo de los atributos a retornar*/
 	Id int,
     Nombre VARCHAR(20)
     )
-INSERT INTO dbo.Puestos
+INSERT INTO dbo.Puestos/*Inserta en la tabla Puestos*/
 SELECT *
-FROM OPENXML (@hdoc, '/Datos/Puestos/Puesto' , 1)
-WITH(
+FROM OPENXML (@hdoc, '/Datos/Puestos/Puesto' , 1)/*Lee los contenidos del XML y para eso necesita un identificador,el PATH del nodo y el 1 que sirve 
+para retornar solo atributos*/
+WITH(/*Dentro del WITH se pone el nombre y el tipo de los atributos a retornar*/
 	Id int,
     Nombre VARCHAR(50),
 	SalarioXHora money,
 	Visible bit
     )
 
-INSERT INTO dbo.Departamentos
+INSERT INTO dbo.Departamentos/*Inserta en la tabla Departamentos*/
 SELECT *
-FROM OPENXML (@hdoc, '/Datos/Departamentos/Departamento' , 1)
-WITH(
+FROM OPENXML (@hdoc, '/Datos/Departamentos/Departamento' , 1)/*Lee los contenidos del XML y para eso necesita un identificador,el PATH del nodo y el 
+1 que sirve para retornar solo atributos*/
+WITH(/*Dentro del WITH se pone el nombre y el tipo de los atributos a retornar*/
 	Id int,
 	IdJefe int,
     Nombre VARCHAR(50)
     )
     
     
-DELETE FROM dbo.Empleados
-DBCC CHECKIDENT ('Empleados', RESEED, 0)
+DELETE FROM dbo.Empleados/*Limpia la tabla empelados*/
+DBCC CHECKIDENT ('Empleados', RESEED, 0)/*Reinicia el identify*/
 
-INSERT INTO dbo.Empleados(Nombre,IdTipoIdentificacion,ValorDocumentoIdentificacion,IdDepartamento,IdPuesto,FechaNacimiento,Visible)
+INSERT INTO dbo.Empleados(Nombre,IdTipoIdentificacion,ValorDocumentoIdentificacion,IdDepartamento,IdPuesto,FechaNacimiento,Visible)/*Inserta en la tabla Empleados*/
 SELECT *
-FROM OPENXML (@hdoc, '/Datos/Empleados/Empleado' , 1)
-WITH(
+FROM OPENXML (@hdoc, '/Datos/Empleados/Empleado' , 1)/*Lee los contenidos del XML y para eso necesita un identificador,el PATH del nodo y el 1 que sirve
+para retornar solo atributos*/
+WITH(/*Dentro del WITH se pone el nombre y el tipo de los atributos a retornar*/
     Nombre VARCHAR(100),
 	IdTipoIdentificacion int,
 	ValorDocumentoIdentificacion VARCHAR(10),
@@ -49,14 +53,15 @@ WITH(
 	Visible bit
     )
  
-INSERT INTO dbo.Administradores(Usuario,Contraseña,Nombre,Tipo)
+INSERT INTO dbo.Administradores(Usuario,Contraseña,Nombre,Tipo)/*Inserta en la tabla Administradores*/
 SELECT *
-FROM OPENXML (@hdoc, '/Datos/Usuarios/Usuario' , 1)
-WITH(
+FROM OPENXML (@hdoc, '/Datos/Usuarios/Usuario' , 1)/*Lee los contenidos del XML y para eso necesita un identificador,el PATH del nodo y el 1 que sirve
+para retornar solo atributos*/
+WITH(/*Dentro del WITH se pone el nombre y el tipo de los atributos a retornar*/
     	Username VARCHAR(50),
 	Pasword VARCHAR(50),
 	Nombre VARCHAR(50),
 	Tipo int
     )
     
-EXEC sp_xml_removedocument @hdoc
+EXEC sp_xml_removedocument @hdoc/*Remueve el documento XML de la memoria*/
