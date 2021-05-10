8145756221ba7187582080a2a96708f2a493f91b
Oswaldo <oswaldo@estudiantec.cr>
2021-05-09 21:53:43 -0600

    Leer Operaciones XML y permisos
    archivo temporal para leer las operaciones por fecha, de momento solo se hace un print de todas por dia, pero ya se lee, falta hacer la logica de semanas y todo eso.
    Ademas ya añadí un sistema de permiso para que si el usuario que inició sesión no tiene cierto rango no puede realizar operaciones criticas

---
+++ SQL Procedures/temp/LeerOperacionesTemp.sql
@@ -0,0 +1,210 @@
+DECLARE @Datos XML/*Declaramos la variable Datos como un tipo XML*/
+ 
+SELECT @Datos = D  /*El select imprime los contenidos del XML para dejarlo cargado en memoria*/
+FROM OPENROWSET (BULK 'C:\Users\Oswaldo\Desktop\Datos_Tarea2_Modificado.xml', SINGLE_BLOB) AS Datos(D)
+DECLARE @hdoc INT /*Creamos hdoc que va a ser un identificador*/
+    
+EXEC sp_xml_preparedocument @hdoc OUTPUT, @Datos/*Toma el identificador y a la variable con el documento y las asocia*/
+
+-- esta tabla es para guardar las operaciones que vamos a hacer, donde cada fila es un dia diferente
+DECLARE @TablaOperaciones TABLE(
+	ID INT IDENTITY(1,1) PRIMARY KEY CLUSTERED, --el id es para cuando hagamos la iteracion de la tabla
+	XmlData XML, --aqui vamos a guardar los nodos de cada operacion para luego dividirlos por categoria
+	Fecha DATE,
+	NuevoEmpleado XML,
+	EliminarEmpleado XML,
+	AsociaEmpleadoConDeduccion XML,
+	DesasociaEmpleadoConDeduccion XML,
+	TipoDeJornadaProximaSemana XML,
+	MarcaDeAsistencia XML
+)
+
+-- guardamos en la tabla de operaciones las operaciones a realizar
+INSERT INTO @TablaOperaciones(
+	XmlData,
+	Fecha,
+	NuevoEmpleado,
+	EliminarEmpleado,
+	AsociaEmpleadoConDeduccion,
+	DesasociaEmpleadoConDeduccion,
+	TipoDeJornadaProximaSemana,
+	MarcaDeAsistencia
+)
+SELECT * 
+FROM OPENXML (@hdoc,'/Datos/Operacion',3)
+WITH (
+	XmlData XML '.',
+	Fecha DATE,
+	NuevoEmpleado XML,
+	EliminarEmpleado XML,
+	AsociaEmpleadoConDeduccion XML,
+	DesasociaEmpleadoConDeduccion XML,
+	TipoDeJornadaProximaSemana XML,
+	MarcaDeAsistencia XML
+)
+
+
+
+DECLARE @CursorTestID INT = 1; --cursor para iterar por la tabla
+
+-- obtenemos la cantidad de filas de la tabla
+DECLARE @RowCnt BIGINT = 0;
+SELECT @RowCnt = COUNT(0) FROM @TablaOperaciones;
+
+DECLARE @main xml = '<root></root>' --plantilla para cuando insertemos las operaciones a las comunas
+
+
+ --iteramos por la tabla
+WHILE @CursorTestID <= @RowCnt
+BEGIN
+	-- en los update lo que hacemos es tomar los nodos con el nombre necesario de cada dia
+	-- y moverlos a la columna necesaria
+	--insertamos las operaciones de insertar empleados
+	UPDATE @TablaOperaciones 
+		SET NuevoEmpleado = (
+				 SELECT @main.query('/root/*'), XmlData.query('/Operacion/NuevoEmpleado') 
+				 FOR XML RAW(''),ROOT('root'), ELEMENTS, TYPE
+				 )
+		WHERE ID = @CursorTestID and NuevoEmpleado IS NOT NULL
+
+	--insertamos las operaciones de eliminar empleados
+	UPDATE @TablaOperaciones
+		SET EliminarEmpleado = (
+				 SELECT @main.query('/root/*'), XmlData.query('/Operacion/EliminarEmpleado') 
+				 FOR XML RAW(''),ROOT('root'), ELEMENTS, TYPE
+				 )
+		WHERE ID = @CursorTestID and EliminarEmpleado IS NOT NULL;
+	
+	--insertamos las operaciones de asociar con deduccion
+	UPDATE @TablaOperaciones
+		SET AsociaEmpleadoConDeduccion = (
+				 SELECT @main.query('/root/*'), XmlData.query('/Operacion/AsociaEmpleadoConDeduccion') 
+				 FOR XML RAW(''),ROOT('root'), ELEMENTS, TYPE
+				 )
+		WHERE ID = @CursorTestID and AsociaEmpleadoConDeduccion IS NOT NULL;
+
+	--insertamos las operaciones de desasociar deduccion
+	UPDATE @TablaOperaciones
+		SET DesasociaEmpleadoConDeduccion = (
+				 SELECT @main.query('/root/*'), XmlData.query('/Operacion/DesasociaEmpleadoConDeduccion') 
+				 FOR XML RAW(''),ROOT('root'), ELEMENTS, TYPE
+				 )
+		WHERE ID = @CursorTestID and DesasociaEmpleadoConDeduccion IS NOT NULL;
+
+	--insertamos las operaciones de tipo jornada
+	UPDATE @TablaOperaciones
+		SET TipoDeJornadaProximaSemana = (
+				 SELECT @main.query('/root/*'), XmlData.query('/Operacion/TipoDeJornadaProximaSemana') 
+				 FOR XML RAW(''),ROOT('root'), ELEMENTS, TYPE
+				 )
+		WHERE ID = @CursorTestID and TipoDeJornadaProximaSemana IS NOT NULL;
+
+	--insertamos las operaciones de marca de asistencia
+	UPDATE @TablaOperaciones
+		SET MarcaDeAsistencia = (
+				 SELECT @main.query('/root/*'), XmlData.query('/Operacion/MarcaDeAsistencia') 
+				 FOR XML RAW(''),ROOT('root'), ELEMENTS, TYPE
+				 )
+		WHERE ID = @CursorTestID and MarcaDeAsistencia IS NOT NULL;
+
+
+	
+	SET @CursorTestID = @CursorTestID + 1 
+END
+
+select * from @TablaOperaciones
+
+
+--reiniciamos todo lo necesario para iterar de nuevo
+set @CursorTestID = 1;
+set  @RowCnt = 0;
+SELECT @RowCnt = COUNT(0) FROM @TablaOperaciones;
+
+declare @subxml xml --subxml para realizar las operaciones de cada columna
+
+-- ahora vamos a iterar de nuevo y realizar las operaciones de cada columna
+WHILE @CursorTestID <= @RowCnt
+BEGIN
+	-- cargamos nuevo empleado en caso de que haya
+	set @subxml = (select TOP 1 NuevoEmpleado FROM @TablaOperaciones WHERE id = @CursorTestID)
+	--si no es nulo realizamos la insercion del empleado
+	if (@subxml is not null)
+		begin
+			EXEC sp_xml_preparedocument @hdoc OUTPUT, @subxml/*Toma el identificador y a la variable con el documento y las asocia*/
+			SELECT * FROM OPENXML (@hdoc,'/root/NuevoEmpleado',3)
+				WITH (
+					FechaNacimiento DATE,
+					Nombre varchar(100),
+					Password varchar(100),
+					Username varchar(100),
+					IdDepartamento int,
+					ValorDocumentoIdentidad int,
+					idPuesto int,
+					idTipoDocumentacionIdentidad int
+				)
+		end
+	-- cargamos eliminar empleado
+	set @subxml = (select TOP 1 EliminarEmpleado FROM @TablaOperaciones WHERE id = @CursorTestID)
+	if @subxml is not null
+		begin
+			EXEC sp_xml_preparedocument @hdoc OUTPUT, @subxml/*Toma el identificador y a la variable con el documento y las asocia*/
+			SELECT * FROM OPENXML (@hdoc,'/root/EliminarEmpleado',3)
+				WITH (
+					ValorDocumentoIdentidad int
+				)
+		end
+
+	-- cargamos asocia empleado con deduccion en caso de que haya
+	set @subxml = (select TOP 1 AsociaEmpleadoConDeduccion FROM @TablaOperaciones WHERE id = @CursorTestID)
+	if @subxml is not null
+		begin
+			EXEC sp_xml_preparedocument @hdoc OUTPUT, @subxml/*Toma el identificador y a la variable con el documento y las asocia*/
+			SELECT * FROM OPENXML (@hdoc,'/root/AsociaEmpleadoConDeduccion',3)
+				WITH (
+					IdDeduccion int,
+					Monto money,
+					ValorDocumentoIdentidad int
+				)
+		end
+
+	-- cargamos desasocia empleado con deduccion en caso de que haya
+	set @subxml = (select TOP 1 DesasociaEmpleadoConDeduccion FROM @TablaOperaciones WHERE id = @CursorTestID)
+	if @subxml is not null
+		begin
+			EXEC sp_xml_preparedocument @hdoc OUTPUT, @subxml/*Toma el identificador y a la variable con el documento y las asocia*/
+			SELECT * FROM OPENXML (@hdoc,'/root/DesasociaEmpleadoConDeduccion',3)
+				WITH (
+					IdDeduccion int,
+					ValorDocumentoIdentidad int
+				)
+		end
+
+	-- cargamos tipo de jornada en caso de que haya
+	set @subxml = (select TOP 1 TipoDeJornadaProximaSemana FROM @TablaOperaciones WHERE id = @CursorTestID)
+	if @subxml is not null
+		begin
+			EXEC sp_xml_preparedocument @hdoc OUTPUT, @subxml/*Toma el identificador y a la variable con el documento y las asocia*/
+			SELECT * FROM OPENXML (@hdoc,'/root/TipoDeJornadaProximaSemana',3)
+				WITH (
+					IdJornada int,
+					ValorDocumentoIdentidad int
+				)
+		end
+
+	-- cargamosmarca de asistencia en caso de que haya
+	set @subxml = (select TOP 1 MarcaDeAsistencia FROM @TablaOperaciones WHERE id = @CursorTestID)
+	if @subxml is not null
+		begin
+			EXEC sp_xml_preparedocument @hdoc OUTPUT, @subxml/*Toma el identificador y a la variable con el documento y las asocia*/
+			SELECT * FROM OPENXML (@hdoc,'/root/MarcaDeAsistencia',3)
+				WITH (
+					FechaEntrada date,
+					FechaSalida date,
+					ValorDocumentoIdentidad int
+				)
+		end
+
+
+	
+	SET @CursorTestID = @CursorTestID + 1 
+END
