f04fab9f8e9a89a2375c4eda4ec62540a458bb2a
FarkleFMR <44482071+FarkleFMR@users.noreply.github.com>
2021-05-16 15:58:06 -0600

    Lo actualize con respecto al nuevo catalogo
    OJO hice cambios en la tabla Feriados y Usuarios convertí sus ID en identity

--- ParseXML-SQL/CatalogoParse.sql
+++ ParseXML-SQL/CatalogoParse.sql
@@ -1,13 +1,13 @@
 DECLARE @Datos xml/*Declaramos la variable Datos como un tipo XML*/
  
 SELECT @Datos = D  /*El select imprime los contenidos del XML para dejarlo cargado en memoria*/
-FROM OPENROWSET (BULK 'D:\CatalogoFinal1.xml', SINGLE_BLOB) AS Datos(D)
+FROM OPENROWSET (BULK 'C:\Users\jenar\OneDrive\Documentos\Datos_Tarea2.xml', SINGLE_BLOB) AS Datos(D)
     
 DECLARE @hdoc int /*Creamos hdoc que va a ser un identificador*/
     
 EXEC sp_xml_preparedocument @hdoc OUTPUT, @Datos/*Toma el identificador y a la variable con el documento y las asocia*/
 
-DELETE FROM dbo.Empleados/*Limpia la tabla empelados*/
+
 DELETE FROM dbo.Puestos/*Limpia la tabla Puestos*/
 
 INSERT INTO dbo.Puestos/*Inserta en la tabla Puestos*/
@@ -44,26 +44,59 @@ WITH(/*Dentro del WITH se pone el nombre y el tipo de los atributos a retornar*/
     Nombre VARCHAR(20)
     )
     
-  
+DELETE FROM dbo.TipoJornada 
 
-DBCC CHECKIDENT ('Empleados', RESEED, 0)/*Reinicia el identify*/
-
-INSERT INTO dbo.Empleados(Nombre,IdTipoIdentificacion,ValorDocumentoIdentificacion,IdDepartamento,IdPuesto,FechaNacimiento,Visible)/*Inserta en la tabla Empleados*/
-SELECT Nombre,idTipoDocumentacionIdentidad,ValorDocumentoIdentidad,IdDepartamento,idPuesto,FechaNacimiento,0
-FROM OPENXML (@hdoc, '/Datos/Empleados/Empleado' , 1)/*Lee los contenidos del XML y para eso necesita un identificador,el PATH del nodo y el 1 que sirve
-para retornar solo atributos*/
+INSERT INTO dbo.TipoJornada(ID,Nombre,HoraEntrada,HoraSalida)
+SELECT Id,Nombre,HoraEntrada,HoraSalida
+FROM OPENXML (@hdoc, '/Datos/Catalogos/TiposDeJornada/TipoDeJornada' , 1)/*Lee los contenidos del XML y para eso necesita un identificador,el 
+PATH del nodo y el 1 que sirve para retornar solo atributos*/
 WITH(/*Dentro del WITH se pone el nombre y el tipo de los atributos a retornar*/
-    Nombre VARCHAR(100),
-	idTipoDocumentacionIdentidad int,
-	ValorDocumentoIdentidad VARCHAR(10),
-	IdDepartamento int,
-	idPuesto int,
-	FechaNacimiento date
-    )
- 
-DELETE FROM dbo.Administradores/*Limpia la tabla Administradores*/
+	HoraEntrada time(7),
+	HoraSalida time(7),
+	Id int,
+	Nombre varchar(128)
+	)
 
-INSERT INTO dbo.Administradores(Usuario,Contraseña,Tipo)/*Inserta en la tabla Administradores*/
+DELETE FROM dbo.TipoMovimiento 
+
+INSERT INTO dbo.TipoMovimiento(ID,Nombre)
+SELECT Id,Nombre
+FROM OPENXML (@hdoc, '/Datos/Catalogos/TiposDeMovimiento/TipoDeMovimiento' , 1)/*Lee los contenidos del XML y para eso necesita un identificador,el 
+PATH del nodo y el 1 que sirve para retornar solo atributos*/
+WITH(/*Dentro del WITH se pone el nombre y el tipo de los atributos a retornar*/
+	Id int,
+	Nombre varchar(128)
+	)
+
+DELETE FROM dbo.Feriados
+DBCC CHECKIDENT ('Feriados', RESEED, 0)/*Reinicia el identify*/
+
+INSERT INTO dbo.Feriados(Fecha,Nombre)
+SELECT Fecha,Nombre 
+FROM OPENXML (@hdoc, '/Datos/Catalogos/Feriados/Feriado' , 1)/*Lee los contenidos del XML y para eso necesita un identificador,el 
+PATH del nodo y el 1 que sirve para retornar solo atributos*/
+WITH(/*Dentro del WITH se pone el nombre y el tipo de los atributos a retornar*/
+	Fecha date,
+	Nombre varchar(128)
+	)
+
+DELETE FROM dbo.TipoDeduccion
+
+INSERT INTO dbo.TipoDeduccion(ID,Nombre,Obligatorio,Porcentual,Valor)
+SELECT *
+FROM OPENXML (@hdoc, '/Datos/Catalogos/Deducciones/TipoDeDeduccion' , 1)/*Lee los contenidos del XML y para eso necesita un identificador,el 
+PATH del nodo y el 1 que sirve para retornar solo atributos*/
+WITH(/*Dentro del WITH se pone el nombre y el tipo de los atributos a retornar*/
+	Id int,
+	Nombre varchar(128),
+	Obligatorio bit,
+	Porcentual bit,
+	Valor money
+	)
+ 
+DELETE FROM dbo.Usuarios/*Limpia la tabla Administradores*/
+
+INSERT INTO dbo.Usuarios(Username,Pwd,Tipo)/*Inserta en la tabla Administradores*/
 SELECT *
 FROM OPENXML (@hdoc, '/Datos/Usuarios/Usuario' , 1)/*Lee los contenidos del XML y para eso necesita un identificador,el PATH del nodo y el 1 que sirve
 para retornar solo atributos*/
