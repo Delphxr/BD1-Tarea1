8fe063e471f5805fb66d3de90b287ebd7d75da4a
Oswaldo <oswaldo@estudiantec.cr>
2021-05-19 18:54:30 -0600

    trigger deduccion obligatoria

--- SQL Procedures/TRIGGERS/dbo.trgAsociarEmpleadoDeduccion.sql
+++ SQL Procedures/TRIGGERS/dbo.trgAsociarEmpleadoDeduccion.sql
@@ -1,21 +1,28 @@
-CREATE TRIGGER dbo.trgAsociarEmpleadoDeduccion
+CREATE TRIGGER [dbo].[trgAsociarEmpleadoDeduccion]
 ON [dbo].[Empleado]
 AFTER INSERT
 AS
 
+
+
 --tabla temporal donde vamos a almacenar las deducciones obligatorias
-DECLARE @t TABLE(IdTipoDeduccion INT)
+DECLARE @t TABLE(IdTipoDeduccion INT,porcentaje money)
 DECLARE @d INT
-SET @d = (SELECT ID FROM inserted)
+
+
+
 
 -- seleccionamos todas las deducciones obligatorias y las ponermos en @t
-INSERT INTO @t (IdTipoDeduccion)
-SELECT [ID]
+INSERT INTO @t (IdTipoDeduccion,porcentaje)
+SELECT [ID],[Valor]
   FROM [dbo].[TipoDeduccion]
   WHERE [Obligatorio] = 1
 
 --insertamos las deducciones obligatorias
 INSERT INTO [dbo].[DeduccionesXEmpleado]
            ([IdEmpleado]
-           ,[IdTipoDeduccion])
-SELECT @d,IdTipoDeduccion FROM @t
+           ,[IdTipoDeduccion]
+		   ,[Monto]
+		   ,[Visible])
+select IsNull(ID,0), t.IdTipoDeduccion, t.porcentaje,1
+from Inserted ,@t t
